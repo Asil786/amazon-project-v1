30d56d18face5c469ea1e10cd6053ff06848d158
Asil786 <www.asilshaikh786@github.com>
2021-09-10 14:12:33 -0700

    save

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line no-unused-vars
+const firebaseConfig = {
+    apiKey: "AIzaSyDlSu53_ogks4UuHcpQRmIofJtwreQZC-Y",
+    authDomain: "clone-fdcec.firebaseapp.com",
+    projectId: "clone-fdcec",
+    storageBucket: "clone-fdcec.appspot.com",
+    messagingSenderId: "32911611711",
+    appId: "1:32911611711:web:f7f6bb80febb78692fa58a"
+  };
