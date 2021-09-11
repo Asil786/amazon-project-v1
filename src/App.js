d757fd361aec16088065d833cfaa2148b8824231
Asil786 <www.asilshaikh786@github.com>
2021-09-10 20:32:51 -0700

    SAVE

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>we are building amazon</h1>
+      <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"/>
     </div>
   );
 }
