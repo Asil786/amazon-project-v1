3033adc125c04469717f72f48ff7c4b9df6f3ed0
Asil786 <www.asilshaikh786@github.com>
2021-09-10 21:22:11 -0700

    save

--- src/Header.js
+++ src/Header.js
@@ -5,10 +5,20 @@ function header() {
     return (
         <div className='header'>
             <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"/>
-
             <div className="header__search">
                 <input className="header__searchInput" type="text"/>
-                {/* Material UI */}
+                {/* Material UI */ }
+            </div>
+            <div className='header__nav'>
+                <div className="header__option">
+
+                </div>
+                <div className="header__option">
+                    
+                </div>
+                <div className="header__option">
+                    
+                </div>
             </div>
         </div>
     )
