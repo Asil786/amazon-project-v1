d42bdeadba96e664dc0c9b26b6b751f118c21744
Asil786 <www.asilshaikh786@github.com>
2021-09-10 21:35:22 -0700

    save

--- src/Header.js
+++ src/Header.js
@@ -4,21 +4,19 @@ import './Header.css'
 function header() {
     return (
         <div className='header'>
-            <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"/>
-            <div className="header__search">
-                <input className="header__searchInput" type="text"/>
-                {/* Material UI */ }
+            <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" 
+            />
+           <div className="header__nav">
+            <div className="header__option">
+                <span className="header__optionLineOne">Hello</span>
+                <span className="header__optionLineOne">Sign In</span>
             </div>
-            <div className='header__nav'>
-                <div className="header__option">
+            <div className="header__option">
+            <span className="header__optionLineOne">Return</span>
+            <span className="header__optionLineOne">&order</span>
+            </div>
+            <div className="header__option">
 
-                </div>
-                <div className="header__option">
-                    
-                </div>
-                <div className="header__option">
-                    
-                </div>
             </div>
         </div>
     )
