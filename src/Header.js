ad6fb4807112480ac22df9923fbeddc4853e7f46
Asil786 <www.asilshaikh786@github.com>
2021-09-10 22:25:50 -0700

    SAVE

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import './Header.css'
 
 function Header() {
     return (
-        <div className='Header'>
+        <div className='header'>
             <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
             />
             <div className="header__search">
@@ -17,7 +17,7 @@ function Header() {
                     <span className='header__optionLineone'> Sign In</span>
                 </div>
                 <div className='header__option'>
-                    <span className='header__optionLinetwo'>&Returns</span>
+                    <span className='header__optionLinetwo'>Returns</span>
                     <span className='header__optionLinetwo'>order</span>
                 </div>
                 <div className='header__option'></div>  
