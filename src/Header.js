e3b578f9e2f670df785ab7ba903d2a8c8436acea
Asil786 <www.asilshaikh786@github.com>
2021-09-10 21:47:06 -0700

    save

--- src/Header.js
+++ src/Header.js
@@ -4,22 +4,8 @@ import './Header.css'
 function header() {
     return (
         <div className='header'>
-            <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" 
-            />
-           <div className="header__nav">
-            <div className="header__option">
-                <span className="header__optionLineOne">Hello</span>
-                <span className="header__optionLineOne">Sign In</span>
-            </div>
-            <div className="header__option">
-            <span className="header__optionLineOne">Return</span>
-            <span className="header__optionLineOne">&order</span>
-            </div>
-            <div className="header__option">
-
-            </div>
+            <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"/>
         </div>
     )
 }
-
 export default header
