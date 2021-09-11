049aed7c717d4b3d165181d39718d99b6b2ed623
Asil786 <www.asilshaikh786@github.com>
2021-09-10 21:53:16 -0700

    save

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import './Header.css'
 
-function header() {
+function Header() {
     return (
         <div className='header'>
-            <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"/>
+            <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+            />
+            <div className="header__search">
+               <input className="header__searchInput" type="text"/>
+               {/* Material UI */}
+            </div>
+
+            <div className="header__nav">
+                <div className='header__option'>
+                    <span className='header__optionLineone'>Hello</span>
+                    <span className='header__optionLineone'> Sign In</span>
+                </div>
+                <div className='header__option'>
+                    <span className='header__optionLinetwo'>Returns</span>
+                    <span className='header__optionLinetwo'>order</span>
+                </div>
+                <div className='header__option'></div>  
+            </div>
         </div>
+
+    
     )
 }
-export default header
+
+export default Header
