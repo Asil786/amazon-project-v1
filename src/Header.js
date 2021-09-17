3184c72193504ce187b43a8915bff2ef38a02c44
Asil786 <www.asilshaikh786@github.com>
2021-09-16 20:58:03 -0700

    save

--- src/Header.js
+++ src/Header.js
@@ -1,31 +1,36 @@
 import React from 'react';
-import './Header.css'
+import './Header.css';
 
 function Header() {
     return (
-        <div className='header'>
-            <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
-            />
-            <div className="header__search">
-               <input className="header__searchInput" type="text"/>
-               {/* Material UI */}
+        <div className="header">
+          <img className="header--logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="amazon"/>
+        
+          <div className="header--search">
+          <input className="header--searchInput" type="text" />
+          {/* Material UI */}
+          </div>  
+
+          <div className="header--nav">
+            <div className="header--option">
+            <span className="header--optionLineOne">Hello Guest</span>
+            <span className="header--optionLineTwo">Sign In</span>
             </div>
 
-            <div className="header__nav">
-                <div className='header__option'>
-                    <span className='header__optionLineone'>Hello</span>
-                    <span className='header__optionLineone'> Sign In</span>
-                </div>
-                <div className='header__option'>
-                    <span className='header__optionLinetwo'>Returns</span>
-                    <span className='header__optionLinetwo'>order</span>
-                </div>
-                <div className='header__option'></div>  
+            <div className="header--option">
+                <span className="header--optionLineOne">Return</span>
+            <span className="header--optionLineTwo">& Order</span>
             </div>
+
+            <div className="header--option">
+                <span className="header--optionLineOne">Your</span>
+            <span className="header--optionLineTwo">Prime</span>
+            </div>
+
+          </div>  
+
         </div>
-
-    
     )
 }
 
-export default Header
+export default Header;
