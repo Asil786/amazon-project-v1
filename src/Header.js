9177bb3b305d7485b978593e79b2ae6cefe7e038
Asil786 <www.asilshaikh786@github.com>
2021-09-18 17:27:32 -0700

    save

--- src/Header.js
+++ src/Header.js
@@ -2,12 +2,19 @@ import React from 'react';
 import './Header.css';
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
 
 function Header() {
     return (
         <div className="header">
           <img className="header--logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="amazon"/>
-        
+          <div className="header--address">
+            <LocationOnOutlinedIcon className="header--adressIcon" />
+            <div className="header--addressline">
+             <span className="header--optionLineOne"> Hello</span>
+             <span className="header--optionLineTwo"> Select your address</span>
+            </div>
+           </div>
           <div className="header--search">
           <input className="header--searchInput" type="text" />
           {/* Material UI */}
