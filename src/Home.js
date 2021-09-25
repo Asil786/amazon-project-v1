ae18436cc22c9157d66206375cfa060c49b0da6d
Asil786 <www.asilshaikh786@github.com>
2021-09-24 21:28:52 -0700

    save

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,18 @@ import './Home.css';
 function Home() {
     return (
         <div className="Home">
-            
+            <div className="home--container">
+              <img className="home--image" src="https://m.media-amazon.com/images/I/71UhgZQzskL._SX3000_.jpg" alt="" />
+            </div> 
+            <div className="home--row">
+
+            </div>
+            <div className="home--row">
+                
+            </div>
+            <div className="home--row">
+                
+            </div>      
         </div>
     )
 }
