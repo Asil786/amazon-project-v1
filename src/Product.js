a343881d9b516c3dc4ad45c187561ae86fc8cb65
Asil786 <www.asilshaikh786@github.com>
2021-09-25 18:50:06 -0700

    save

--- src/Product.js
+++ src/Product.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import './Product.css'
+import'./Product.css';
 
 function Product() {
     return (
-        <div>
-            
+        <div className="product">
+            <div className="product--info">
+            <p>See U in C</p>
+            <p>by Ali Karim Sayed (Author)</p>
+
+            <p className="product--price">
+            <bold>₹</bold>
+            <strong>4424.56</strong>
+            </p>
+                
+             <img src="https://images-na.ssl-images-amazon.com/images/I/41SMlI+1PrL._SX331_BO1,204,203,200_.jpg" alt="" /> 
+
+             <button>Add to Cart</button>  
+            </div>
         </div>
     )
 }
