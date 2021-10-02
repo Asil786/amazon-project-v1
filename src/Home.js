2cfe132697ba08077b8906c4078e48ad6c04412f
Asil786 <www.asilshaikh786@github.com>
2021-10-01 21:12:32 -0700

    save

--- src/Home.js
+++ src/Home.js
@@ -9,7 +9,12 @@ function Home() {
             <img className="home--image" src="https://m.media-amazon.com/images/I/71i723QTb+L._SX3000_.jpg" alt="" />
 
             <div className="home--row">
-            <Product />
+            <Product 
+               id="01"
+               title="See U in C by Ali Karim Sayed (Author)"
+               price={4450}
+               image="https://images-na.ssl-images-amazon.com/images/I/41SMlI+1PrL._SX331_BO1,204,203,200_.jpg"
+            />
             <Product />
             <Product />
             </div>
--- src/Product.js
+++ src/Product.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import './Product.css';
 
-function Product({props}) {
+function Product({id, title, price, image}) {
     return (
         <div className="product">
           <div className="product--info">
-            <p>See U in C</p>
-            <p>by Ali Karim Sayed (Author)</p>
+            <P>{title}</P>
 
             <p className="product--price">
             <bold>₹</bold>
-            <strong>4424.56</strong>
+            <strong>{price}</strong>
             </p>
                 
-             <img className="product--image" src="https://images-na.ssl-images-amazon.com/images/I/41SMlI+1PrL._SX331_BO1,204,203,200_.jpg" alt="" /> 
+             <img className="product--image" src={Image} alt="" /> 
           </div>
              <button className="product--button">Add to Cart</button>  
           
